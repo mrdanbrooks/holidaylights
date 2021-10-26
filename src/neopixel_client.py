edd2c5ec4c0e59ddd2a34f9182a5cd0b89773549
dan brooks <dan@danbrooks.net>
2021-10-25 22:55:04 -0400

    makes pixel values private in neopixel client

--- src/neopixel_client.py
+++ src/neopixel_client.py
@@ -4,14 +4,12 @@ import neopixel
 class NeoPixelClient:
     def __init__(self, num_pixels):
         self.num_pixels = num_pixels
-#         self.pixels = neopixel.NeoPixel(board.D18, num_pixels, auto_write=False) # pixel_order = neopixel.GRB
-        self.pixels = neopixel.NeoPixel(board.D18, num_pixels, auto_write=False, pixel_order = neopixel.GRB)
+#         self._pixels = neopixel.NeoPixel(board.D18, num_pixels, auto_write=False) # pixel_order = neopixel.GRB
+        self._pixels = neopixel.NeoPixel(board.D18, num_pixels, auto_write=False, pixel_order = neopixel.RGB)
 
     def put_pixels(self, led_values):
-            print("ledvals = %s" % led_values)
             for i in range(self.num_pixels):
-                self.pixels[i] = led_values[i]
-            self.pixels.show()
-            print("pixels = %s" % self.pixels)
+                self._pixels[i] = led_values[i]
+            self._pixels.show()
 
 
